71d1594066a639a3a070714069d9ef45c1500772
Djoulzy <29226401+Djoulzy@users.noreply.github.com>
2021-10-07 16:29:46 +0200

    test couleurs

--- DHGR/editor/colors.js
+++ DHGR/editor/colors.js
@@ -1,42 +1,6 @@
-var colorValues = {
-    blck: {val: 0, bin: "0000"},
-    dkbl: {val: 1, bin: "0001"},
-    dkgr: {val: 2, bin: "0010"},
-    mdbl: {val: 3, bin: "0011"},
-    brwn: {val: 4, bin: "0100"},
-    gry2: {val: 5, bin: "0101"},
-    gren: {val: 6, bin: "0110"},
-    aqua: {val: 7, bin: "0111"},
-    mgta: {val: 9, bin: "1001"},
-    gry1: {val: 10, bin: "1010"},
-    ltbl: {val: 11, bin: "1011"},
-    orge: {val: 12, bin: "1100"},
-    pink: {val: 13, bin: "1101"},
-    ylow: {val: 14, bin: "1110"},
-    whte: {val: 15, bin: "1111"},
-}
-
-var altColorValues = {
-    blck: {val: 0, bin: "0000"},
-    dkbl: {val: 1, bin: "0100"},
-    dkgr: {val: 2, bin: "0010"},
-    mdbl: {val: 3, bin: "1100"},
-    brwn: {val: 4, bin: "0001"},
-    gry2: {val: 5, bin: "0101"},
-    gren: {val: 6, bin: "1001"},
-    aqua: {val: 7, bin: "1101"},
-    mgta: {val: 9, bin: "0110"},
-    gry1: {val: 10, bin: "1010"},
-    ltbl: {val: 11, bin: "1110"},
-    orge: {val: 12, bin: "0011"},
-    pink: {val: 13, bin: "0111"},
-    ylow: {val: 14, bin: "1011"},
-    whte: {val: 15, bin: "1111"},
-}
-
 var colors = [
     {
-        id: 0,
+        id: 0, // BLACK
         2: {
             0: {class: "blck", bin: "00"},
             1: {class: "blck", bin: "00"},
@@ -49,39 +13,55 @@ var colors = [
         }
     },
     {
-        id: 1,
+        id: 1, // MAGENTA / RED
         2: {},
         4: {
-            0: {class: "dkbl", bin: "0001"},
-            1: {class: "dkbl", bin: "0001"},
-            alt: {class: "dkbl", bin: "0100"},
+            0: {class: "red", bin: "1000"},
+            1: {class: "red", bin: "1000"},
+            alt: {class: "red", bin: "0010"},
         }
     },
     {
-        id: 2,
+        id: 2, // BROWN
         2: {},
-        4: {}
+        4: {
+            0: {class: "brwn", bin: "0100"},
+            1: {class: "brwn", bin: "0100"},
+            alt: {class: "brwn", bin: "0001"},
+        }
     },
     {
-        id: 3,
+        id: 3, // ORANGE
         2: {},
-        4: {}
+        4: {
+            0: {class: "orng", bin: "1100"},
+            1: {class: "orng", bin: "1100"},
+            alt: {class: "orng", bin: "0111"},
+        }
     },
     {
-        id: 4,
+        id: 4, // DARK GREEN
         2: {},
-        4: {}
+        4: {
+            0: {class: "dgrn", bin: "0010"},
+            1: {class: "dgrn", bin: "0010"},
+            alt: {class: "dgrn", bin: "0001"},
+        }
     },
     {
-        id: 5,
+        id: 5, // GREY 1 / DARK GREY
         2: {},
-        4: {}
+        4: {
+            0: {class: "gry1", bin: "1010"},
+            1: {class: "gry1", bin: "1010"},
+            alt: {class: "gry1", bin: "1010"},
+        }
     },
     {
-        id: 6,
+        id: 6, // GREEN
         2: {
             0: {class: "gren", bin: "10"},
-            1: {class: "orge", bin: "10"},
+            1: {class: "orng", bin: "10"},
             alt: {}
         },
         4: {
@@ -91,26 +71,136 @@ var colors = [
         }
     },
     {
-        id: 7,
+        id: 7, // YELLOW
         2: {},
-        4: {}
+        4: {
+            0: {class: "ylow", bin: "1110"},
+            1: {class: "ylow", bin: "1110"},
+            alt: {class: "ylow", bin: "1011"},
+        }
     },
     {
-        id: 8,
+        id: 8, // DARK BLUE
         2: {},
-        4: {}
+        4: {
+            0: {class: "dblu", bin: "0001"},
+            1: {class: "dblu", bin: "0001"},
+            alt: {class: "dblu", bin: "1000"},
+        }
     },
     {
-        id: 9,
+        id: 9, // VIOLET / PURPLE
         2: {
-            0: {class: "mgta", bin: "01"},
-            1: {class: "mdbl", bin: "01"},
+            0: {class: "prpl", bin: "01"},
+            1: {class: "mblu", bin: "01"},
             alt: {}
         },
         4: {
-            0: {class: "mgta", bin: "1001"},
-            1: {class: "mgta", bin: "1001"},
-            alt: {class: "mgta", bin: "0110"},
+            0: {class: "prpl", bin: "1001"},
+            1: {class: "prpl", bin: "1001"},
+            alt: {class: "prpl", bin: "0110"},
+        }
+    },
+    {
+        id: 10, // GREY 2
+        2: {},
+        4: {
+            0: {class: "gry2", bin: "0101"},
+            1: {class: "gry2", bin: "0101"},
+            alt: {class: "gry2", bin: "1010"},
+        }
+    },
+    {
+        id: 11, // PINK
+        2: {},
+        4: {
+            0: {class: "pink", bin: "1101"},
+            1: {class: "pink", bin: "1101"},
+            alt: {class: "pink", bin: "0111"},
+        }
+    },
+    {
+        id: 12, // MEDIUM BLUE
+        2: {},
+        4: {
+            0: {class: "mblu", bin: "0011"},
+            1: {class: "mblu", bin: "0011"},
+            alt: {class: "mblu", bin: "0011"},
+        }
+    },
+    {
+        id: 13, // LIGHT BLUE
+        2: {},
+        4: {
+            0: {class: "lblu", bin: "1101"},
+            1: {class: "lblu", bin: "1101"},
+            alt: {class: "lblu", bin: "1110"},
+        }
+    },
+    {
+        id: 14, // AQUA
+        2: {},
+        4: {
+            0: {class: "aqua", bin: "0111"},
+            1: {class: "aqua", bin: "0111"},
+            alt: {class: "aqua", bin: "1101"},
+        }
+    },
+    {
+        id: 15, // WHITE
+        2: {
+            0: {class: "whte", bin: "11"},
+            1: {class: "whte", bin: "11"},
+            alt: {}
+        },
+        4: {
+            0: {class: "whte", bin: "1111"},
+            1: {class: "whte", bin: "1111"},
+            alt: {class: "whte", bin: "1111"},
         }
     }
 ]
+
+colors.get = function(index, mode, palette) {
+    return this[index][mode][palette]
+}
+
+colors.displayPalette = function(tagId, mode) {
+    switch(mode) {
+        case 2:
+            let hgrColors = [0, 6, 9, 15]
+            hgrColors.forEach(function(value) {
+                let colorValue = colors.get(value, 2, 0)
+                let elmt = document.createElement('div')
+                elmt.id = "ColorCell_" + value + "_"
+                if (value == 6 || value == 9) {
+                    elmt.id += 0
+                    elmt.paletteIndex = 0
+                }
+                elmt.className = "paletteCell " + colorValue.class
+                elmt.colorIndex = value
+                tagId.appendChild(elmt)
+                if (value == 6 || value == 9) {
+                    let colorValue = colors.get(value, 2, 1)
+                    let elmt = document.createElement('div')
+                    elmt.id = "ColorCell_" + value + "_" + 1
+                    elmt.className = "paletteCell " + colorValue.class
+                    elmt.colorIndex = value
+                    elmt.paletteIndex = 1
+                    tagId.appendChild(elmt)
+                }
+            })
+            break
+        case 4:
+            for(var i=0; i<16; i++) {
+                let colorValue = colors.get(i, 4, 0)
+                let elmt = document.createElement('div')
+                elmt.id = "ColorCell_" + i + "_" + 0
+                elmt.className = "paletteCell " + colorValue.class
+                elmt.colorIndex = i
+                elmt.paletteIndex = 0
+                tagId.appendChild(elmt)
+            }
+            break
+    }
+}
--- DHGR/editor/editor2.js
+++ DHGR/editor/editor2.js
@@ -1,11 +1,19 @@
 
 const NB_PIXELS_BY_PACK = 7
+const ALT_MODE = true
+
+var SelectedColorIndex = 0
+var SelectedPalette = 0
 
 function isset(variable) {
     if(typeof(variable) != "undefined" && variable !== null) {
         return true
     }
     return false
+}
+
+function getHex(val) {
+    return val.toString(16).padStart(2, '0').toUpperCase()
 }
 
 class Pack
@@ -39,7 +47,6 @@ class Pack
     }
 
     setColor(pos, val, pal) {
-        console.log(val)
         this.stack[pos] = val
         let paletIndex = this.getPaletteIndex(pos)
         if (isset(pal)) {
@@ -52,34 +59,48 @@ class Pack
         this.updatePack()
     }
 
-    get() {
-        var res = ""
+    get(format, altMode = false) {
+        var res = [[],[]]
+        var memSelector = 0
         var octect = ""
         var bits = 0
+        var computedValue = ""
+
         for(var i=0; i<this.pixelsInPack; i++) {
+            let paletteValue = this.palette[this.getPaletteIndex(i)]
+            let colorValue
+            if (altMode) colorValue = this.stack[i][this.bitsPerPixel]["alt"]
+            else colorValue = this.stack[i][this.bitsPerPixel][paletteValue]
             if (bits+this.bitsPerPixel > NB_PIXELS_BY_PACK) {
                 let nb = 7 - bits
-                octect = this.stack[i].substr(-nb) + octect
 
-                res += " " + this.palette[this.getPaletteIndex(i)] + octect
+                octect = colorValue.bin.substr(-nb) + octect
+                computedValue = paletteValue + octect
+
+                if (format == "bin") res[memSelector].push("#%"+ computedValue)
+                else res[memSelector].push(getHex(computedValue))
+                memSelector = Math.abs(memSelector-1)
 
                 bits = this.bitsPerPixel - nb
-                octect = this.stack[i].substr(0, bits)
+                octect = colorValue.bin.substr(0, bits)
             }  
             else {
-                if (isset(this.stack[i])) octect = this.stack[i] + octect
-                else octect = "--" + octect
+                octect = colorValue.bin + octect
                 bits += this.bitsPerPixel
             }
         }
-        res += " " + this.palette[this.getPaletteIndex(i-1)] + octect
+
+        computedValue = this.palette[this.getPaletteIndex(i-1)] + octect
+        if (format == "bin") res[memSelector].push("#%"+computedValue)
+        else res[memSelector].push(getHex(computedValue))
+        console.log(res)
         return res
     }
 
     render(parent) {
         if (!isset(parent)) parent = document.body
         for(var i=0; i<this.pixelsInPack; i++) {
-            this.element[i] = document.createElement('div');
+            this.element[i] = document.createElement('div')
             this.element[i].pack = this
             this.element[i].posInPack = i
             parent.appendChild(this.element[i]);
@@ -90,49 +111,108 @@ class Pack
 
 class Sprite
 {
-    constructor(type, width, height) {
+    constructor(type, width, height, name) {
+        this.width = width
+        this.height = height
+        this.name = name.toUpperCase()
         this.nbPackPerLine = Math.round(width / NB_PIXELS_BY_PACK)
         this.totalPacks = this.nbPackPerLine * height
         this.grid = new Array(this.totalPacks)
+        
+        this.mode = 2
+        
         switch(type) {
             case 'mono': break
             case 'hgr':
                 for(let i=0; i<this.totalPacks; i++) this.grid[i] = new Pack(2)
+                this.mode = 2
                 break
             case 'dhgr':
                 for(let i=0; i<this.totalPacks; i++) this.grid[i] = new Pack(4)
+                this.mode = 4
                 break
         }
     }
 
-    get() {
-        var tmp = "";
+    get(format) {
+        let pixWidth = (this.width / NB_PIXELS_BY_PACK) * 2
+        let totalBytes = pixWidth * this.height
+
+        let macro = ""
+        if (format == "bin") macro = "DA"
+        else macro = "HS"
+
+        var tmp = this.name+"<br/>"
+        var aux = ""
+        var main = ""
+        tmp += "&nbsp;&nbsp;&nbsp;&nbsp;.HS " + getHex(pixWidth) + "," + getHex(this.height) + "," + getHex(totalBytes) + "<br/>"
+        tmp += "&nbsp;&nbsp;&nbsp;&nbsp;.DA #"+this.name+"_ALT,/"+this.name+"_ALT<br/>"
         this.grid.forEach(function(value) {
-            tmp += value.get()
+            let res = value.get(format)
+            aux += "&nbsp;&nbsp;&nbsp;&nbsp;." + macro + " " + res[0] + "<br/>"
+            main += "&nbsp;&nbsp;&nbsp;&nbsp;." + macro + " " + res[1] + "<br/>"
         })
+        tmp += aux + main
+
+        var aux = ""
+        var main = ""
+        tmp += "<br/>"+this.name+"_ALT<br/>"
+        tmp += "&nbsp;&nbsp;&nbsp;&nbsp;.HS " + getHex(pixWidth) + "," + getHex(this.height) + "," + getHex(totalBytes) + "<br/>"
+        tmp += "&nbsp;&nbsp;&nbsp;&nbsp;.DA #"+this.name+",/"+this.name+"" + "<br/>"
+        this.grid.forEach(function(value) {
+            let res = value.get(format, ALT_MODE)
+            aux += "&nbsp;&nbsp;&nbsp;&nbsp;." + macro + " " + res[0] + "<br/>"
+            main += "&nbsp;&nbsp;&nbsp;&nbsp;." + macro + " " + res[1] + "<br/>"
+        })
+        tmp += aux + main
         return tmp
     }
 
-    render() {
+    render(tagId) {
         var parent = this;
         this.grid.forEach(function(value, index) {
-            value.render()
-            if (index % parent.nbPackPerLine == 0) {
-                var elemt = document.createElement('br');
-                document.body.appendChild(elemt);
+            value.render(tagId)
+            if ((index+1) % parent.nbPackPerLine == 0) {
+                var elemt = document.createElement('div');
+                elemt.className = "clearBoth"
+                tagId.appendChild(elemt);
             }
         })
     }
+
+    displayPalette(tagId) {
+        colors.displayPalette(tagId, this.mode)
+    }
 }
 
-$(document).ready(function() {
-    let sprite1 = new Sprite("hgr", 7, 8)
-    sprite1.render()
+$(document).ready(function()
+{
+    var draw = document.getElementById("drawzone")
+    var pal = document.getElementById("palette")
+
+    let sprite1 = new Sprite("dhgr", 7, 16, "name")
+    sprite1.render(draw)
+    sprite1.displayPalette(pal)
 
     $(".pixel").on("click", function() {
         let packObject = $(this)[0].pack
         let pos = $(this)[0].posInPack
-        packObject.setColor(pos, colors['9'], 0)
+        packObject.setColor(pos, colors[SelectedColorIndex], SelectedPalette)
+    })
+
+    $(".paletteCell").on("click", function() {
+        let id = "#ColorCell_"+SelectedColorIndex+"_"+SelectedPalette
+        $(id).removeClass("selected")
+        SelectedColorIndex = $(this)[0].colorIndex
+        SelectedPalette = $(this)[0].paletteIndex
+        id = "#ColorCell_"+SelectedColorIndex+"_"+SelectedPalette
+        $(id).addClass("selected")
+    })
+
+    $("#compute").on("click", function() {
+        let format = $('input[name=format]:checked').val()
+        let tmp = sprite1.get(format)
+        $("#output").html(tmp)
     })
 })
 
